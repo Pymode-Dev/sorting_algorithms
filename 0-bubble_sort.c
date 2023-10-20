5a3e961f7f32dd502a7b270ea5e5ffb93e283672
Pymode-Dev <pymode000@gmail.com>
2023-10-20 16:25:29 -0700

    Fix docstring

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,6 +2,27 @@
 
 
 /**
+ * swap_arr - This swap elements in an array
+ *
+ * @array: the array to manipulate
+ * @first: the first element index to change
+ * @second: the second element index to change
+ * Return: void
+ */
+
+void swap_arr(int *array, size_t first, size_t second)
+{
+	size_t f, s;
+
+	f = array[first];
+	s = array[second];
+
+	array[first] = s;
+	array[second] = f;
+
+}
+
+/**
  * bubble_sort - function that sorts an array of integers in ascending order
  *
  * @array: the array to sort
