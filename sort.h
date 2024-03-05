75c9b4bf3e13af2f25e436b664de3aad1255b9d0
Pymode-Dev <pymode000@gmail.com>
2024-03-05 15:10:32 -0800

    0. Bubble sort

--- sort.h
+++ sort.h
@@ -34,9 +34,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* swap */
-void swap_ints(int *a, int *b);
-
 
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
